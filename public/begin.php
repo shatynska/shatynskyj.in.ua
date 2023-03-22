1b64049675523ad7a76dec0433a1609365bf05d1
Helen Shatynska <helen.shatynska@gmail.com>
2023-03-22 23:34:43 +0200

    Add  header background

--- public/begin.php
+++ public/begin.php
@@ -2,23 +2,25 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1"/>
-	 <title>Художня ковка: флюгера, шпилі, огорожі, балкони: Шатинський Володимир. Дрогобич.</title>
-	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> 
-	 <link rel="stylesheet" href="css/style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Художня ковка: флюгера, шпилі, огорожі, балкони: Шатинський Володимир. Дрогобич.</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body class="container text-center">
-<header class="m-3">
-	<h1>Художня ковка</h1>
-	<h2>флюгера, шпилі, огорожі, балкони ...</h2>
-	<div>
-		Шатинський Володимир<br />
-		м. Дрогобич<br />
-		<h3><a href="tel:+380689611663" id="tel">+380689611663</a><br /></h3>
-		<a href="mailto:shatynskyj@gmail.com">shatynskyj@gmail.com</a>
-	</div>
-</header>
+	<img src="images/sky-fon.jpg" alt="">
+	<header class="m-3">
 
-<main>
+		<h1>Художня ковка</h1>
+		<h2>флюгера, шпилі, огорожі, балкони ...</h2>
+		<div>
+			Шатинський Володимир<br />
+			м. Дрогобич<br />
+			<h3><a href="tel:+380689611663" id="tel">+380689611663</a><br /></h3>
+			<a href="mailto:shatynskyj@gmail.com">shatynskyj@gmail.com</a>
+		</div>
+	</header>
+
+	<main>
