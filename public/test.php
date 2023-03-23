b205d11e4a153b5ff79e376918ff1abc834c515b
Helen Shatynska <helen.shatynska@gmail.com>
2023-03-23 23:23:01 +0200

    Add test.php page for new version of site

---
+++ public/test.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Художня ковка: флюгера, шпилі, огорожі, балкони: Шатинський Володимир. Дрогобич.</title>
+	<link rel="stylesheet" href="css/style.css">
+	<style>
+		body {
+			padding: 0;
+			margin: 0;
+		}
+
+		.container {
+			width: 1420px;
+			margin: 0 auto;
+			display: grid;
+			grid-template-areas: 'top top top' 'left center right' 'footer footer footer';
+			grid-template-columns: 1fr 300px 1fr;
+			grid-template-rows: 320px minmax(300px, auto) 100px;
+		}
+
+		.top {
+			grid-area: top;
+			margin: 0 auto;
+			background: no-repeat center url("images/sky-fon.jpg");
+		}
+
+		header {
+			grid-area: center;
+			text-align: center;
+		}
+
+		footer {
+			grid-area: footer;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="container">
+		<div class="top">
+			<img src="images/sky-fon.jpg" alt="" srcset="">
+		</div>
+
+		<header>
+			<h1>Художня ковка</h1>
+			<h2>флюгера, шпилі, огорожі, балкони ...</h2>
+			<div>
+				Шатинський Володимир<br />
+				м. Дрогобич<br />
+				<h3><a href="tel:+380689611663" id="tel">+380689611663</a><br /></h3>
+				<a href="mailto:shatynskyj@gmail.com">shatynskyj@gmail.com</a>
+			</div>
+		</header>
+
+		<main class="left">
+		</main>
+
+		<footer>
+		</footer>
+	</div>
+
+</body>
+
+</html>
