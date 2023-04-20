857c947011d69cb0bf48d7ca4be221a1f687f252
Helen Shatynska <helen.shatynska@gmail.com>
2023-04-20 15:13:35 +0300

    Add modal layer for gallery

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -5,5 +5,34 @@ let imageSrc;
 images.forEach((image) => {
   image.addEventListener("click", (e) => {
     imageSrc = e.target.src;
+    imageModal(imageSrc);
   });
 });
+
+//creating the modal
+let imageModal = (src) => {
+  const modal = document.createElement("div");
+  modal.setAttribute("class", "modal");
+  document.querySelector(".container").append(modal);
+
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", src);
+
+  //creating the close button
+  const closeButton = document.createElement("div");
+  closeButton.setAttribute("class", "closeButton");
+  closeButton.innerText = "x";
+
+  //close functions
+  closeButton.onclick = () => {
+    modal.remove();
+  };
+
+  modal.addEventListener("click", function (e) {
+    if (e.target.tagName != "IMG") {
+      modal.remove();
+    }
+  });
+
+  modal.append(newImage, closeButton);
+};
