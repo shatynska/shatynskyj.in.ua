676b70b1910213dc13f39d6f4eac8977740e6618
Helen Shatynska <helen.shatynska@gmail.com>
2023-04-30 16:47:53 +0300

    Add google tag

--- public/index.php
+++ public/index.php
@@ -12,18 +12,32 @@ require_once('../app.php');
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	
 	<title>Художня ковка: флюгера, шпилі, ворота, огорожі та інші ковані вироби. Замовити в Дрогобичі.</title>
 	<meta name="description" content="Виготовлення на замовлення кованих скульптур та інших виробів з металу, зокрема флюгерів та шпилів. Художня обробка металу. &#9743; 8 (068) 961 16 63. Дрогобич" />
+	
 	<link rel="icon" href="favicon.ico">
 	<link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
 	<link rel="manifest" href="/images/favicons/site.webmanifest">
+	
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Manrope&family=Russo+One&display=swap" rel="stylesheet">
+	
 	<link rel="stylesheet" href="css/style.css">
+	
 	<script defer src="js/gallery.js"></script>
+	
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-YZHKL87ZP3"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-YZHKL87ZP3');
+	</script>
 </head>
 
 <body>
